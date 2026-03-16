00561fccf2e6a01dba375ef44e7b9469c524d6a1
aniruddha-chaudhari <aniruddhachaudhari2704@gmail.com>
2026-03-16 20:42:24 +0530

    chore: Update OpenClaw project details, modifying description and tags for improved accuracy.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import { Doto } from 'next/font/google'
 import ClientProviders from '@/components/client-providers'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 const doto = Doto({
@@ -39,6 +40,7 @@ html {
         <ClientProviders>
           {children}
           <Analytics />
+          <SpeedInsights />
         </ClientProviders>
       </body>
     </html>
